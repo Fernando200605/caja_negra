5b9173df5631658f2f998595af3b5c10627b5d44
Luis Munevar Solano <luismunevars500@gmail.com>
2025-12-01 16:05:08 -0500

    Agregacion urls en la app

--- CajaNegra/app/urls.py
+++ CajaNegra/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from app.views import * 
 
 urlpatterns = [
-    
+    path('habitacion/1/', views.habitacion_inicial, name='habitacion'),
+    path('mover/<str:direccion>/', views.mover, name='mover'),
 ]
