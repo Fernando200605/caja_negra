12cdbd9f6f56f690651e309d0c9194cb1c496f5a
Luis Munevar Solano <luismunevars500@gmail.com>
2025-12-01 16:06:20 -0500

    Creacion de las vistas act 3

--- CajaNegra/app/views.py
+++ CajaNegra/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def habitacion_inicial(request):
+    return HttpResponse(
+        "Estás en un pasillo oscuro. Hay puertas al 'norte' y al 'sur'. "
+        "Escribe la ruta juego/mover/norte o juego/mover/sur"
+    )
+
+def mover(request, direccion):
+    if direccion == "norte":
+        return HttpResponse("Caíste en un pozo. Fin del juego.")
+    elif direccion == "sur":
+        return HttpResponse("Encontraste una puerta secreta. ¡Ganaste!")
+    else:
+        return HttpResponse("Esa dirección no existe. Usa norte o sur.")
