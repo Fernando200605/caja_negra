6ce817ae8a45c7ceabb7c64f694c15c92309f9b7
JuanGuillermoPerezVega <jperezv2@soy.sena.edu.co>
2025-12-01 13:56:05 -0500

    Se creo la calculadora

--- CajaNegra/app/urls.py
+++ CajaNegra/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from app.views import * 
 
 urlpatterns = [
-    
+    path('calculadora/<str:operacion>/<int:a>/<int:b>/', calculadora),
 ]
--- CajaNegra/app/views.py
+++ CajaNegra/app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, Http404
+
+
+def calculadora(request, operacion, a, b):
+
+    if operacion == "suma":
+        resultado = a + b
+
+    elif operacion == "resta":
+        resultado = a - b
+
+    elif operacion == "multiplicacion":
+        resultado = a * b
+
+    elif operacion == "division":
+        if b == 0:
+            return HttpResponse("Error: división por cero")
+        resultado = a / b
+
+    else:
+        raise Http404("Error 404 manual: Operación no encontrada")
+
+    return HttpResponse(f"El resultado es {resultado}")
+
 
 # Create your views here.
