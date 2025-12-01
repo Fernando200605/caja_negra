98e7521a0d5e404e49230ac041cfb197d266cc39
Fernando200605 <121266068+Fernando200605@users.noreply.github.com>
2025-12-01 17:28:05 -0500

    edición menor

--- CajaNegra/app/urls.py
+++ CajaNegra/app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from app.views import * 
 
 urlpatterns = [
-    path('habitacion/1/', views.habitacion_inicial, name='habitacion'),
-    path('mover/<str:direccion>/', views.mover, name='mover'),
+    path('habitacion/1/', habitacion_inicial, name='habitacion'),
+    path('mover/<str:direccion>/', mover, name='mover'),
 ]
